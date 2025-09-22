9aafb07bd937da9bac9de7c197c03e7d65a74f9d
Skues <alfieskues2006@gmail.com>
2025-09-22 13:19:41 +0100

    Split up project details and finalising CSS

--- my-react-app/src/Home.tsx
+++ my-react-app/src/Home.tsx
@@ -50,7 +50,6 @@ function AboutMe() {
                     {/* <p>add bullet point list of interests</p> */}
                     {/* <img src='/pictures/coraline-dad-type.gif' alt='meIRL' width={100} height={100} /> */}
                     <p>I enjoy solving my own problems with code and constantly learning new tools, languages and techniques. Outside of programming, I am a bodybuilder and ex top 100 Brawlhalla player.</p>
-                    <p>Write what you enjoy doing, why you enjoy programming, type of things you made, what do you do in your free time</p>
                 </div>
                 <div className='imageHover'></div>
                 {/* // Credit to my beautiful girlfriend for drawing this */}
@@ -67,23 +66,62 @@ function ProjectsPreview() {
             <ul className='projectList'>
                 <li className='individualProject'>
                     <h2>Fitness App (change name)</h2>
-                    <p>Full stack fitness progression mobile application, which allows users to log workouts, visualise progress and see potential predictions through a custom machine learning model. Built using React Native for compatible front end, created a RESTful Flask API to handle endpoints and data transfer. Python's pandas, scikit-learn and numPy were all used for creating the model and synthetic data (more info on the GitHub README). SQLAlchemy database.</p>
-                    <a href='https://github.com/Skues/FitnessApp' target='_blank'>Link</a>
+                    <p>Full stack mobile fitness progression to track workouts, visualise progress, and predict future performance using machine learning.</ p>
+                    <ul>
+                        <li>Built using React Native for compatible front end</li>
+                        <li>Developed a RESTful API using Flask to handle endpoints and data transfer.</ li>
+                        <li>Python's pandas, scikit-learn and numPy were all used for creating the model and synthetic data (more info on the GitHub README)</li>
+                        <li>SQLAlchemy database.</li>
+                    </ul>
+                    <br />
+                    <a className="linkButton" href='https://github.com/Skues/FitnessApp' target='_blank'>Link</a>
                 </li>
                 <li className='individualProject'>
                     <h2>YourWeather (change name)</h2>
-                    <p>Web application which gathers real-time weather data from an API and does basic weather app features but with the aim of providing advice towards the user's sleep, based on their preferences. For example, if the user likes a cooler room when they sleep, the app will tell the user the state of the weather during their typical sleeping time, then give advice on how to mitigate certain weather conditions. Backend is made with Python's Flask and the front end is built entirely with HTML and CSS. Integrate with a MySQL server to store user data. </p>
-                    <a href='https://github.com/Skues/YourWeather' target='_blank'>Link</a>
+                    <p>Web application that uses real-time weather data to provide advice targetted at getting better sleep, based on user preferences. </p>
+                    <ul>
+                        <li>Retrieves detailed weather data from a public API.</li>
+                        <li>Analyses conditions during the user's sleep hours and provides advice.</li>
+                        <li>Backend built using <b>Python Flask</b>.</li>
+                        <li>Frontend developed with <b>HTML</ b> and <b>CSS</ b>.</li>
+                        <li>Integrated with a <b>MySQL</b > database to store and retrieve user preferences.</li>
+                        <li>Hosted on <b>Docker</b> .</li>
+
+                    </ul>
+                    <br />
+                    <a className="linkButton" href='https://github.com/Skues/YourWeather' target='_blank'>Link</a>
                 </li>
                 <li className='individualProject'>
                     <h2>AI Train Chatbot</h2>
-                    <p>Lead a successful group university project with the aim to create an intelligent chatbot for a train operating company. The two main objectives were to help customers find the cheapest available ticket for their intended journey, in the UK. And to estimate the delay of a train journey using a machine learning model, trained on 3 years of train data. Backend and frontend were built in Python with spaCy for NLP, Selenium for scraping, and scikit-learn for the ML model.</p>
-                    <a href='https://github.com/Skues/AI-Train-Chatbot' target='_blank'>Link</a>
+                    <p>Led a successful university group project to build an AI chatbot for a UK train operating company.</p>
+                    <ul><li>The two main aims were:</li>
+                        <ul>
+                            <li>Help customers find the cheapest available ticket for their journey.</li>
+                            <li>Estimating delays using a machine learning model trained on 3 years of historical data.</li>
+                        </ul>
+                        <li>Developed both frontend and backend with Python.</li>
+                        <li>Used spaCy for NLP with a custom spaCy model to recognise train stations.</li>
+                        <li>Selenium was used to scrape real-time train station data.</li>
+                        <li>Built and trained the delay prediction model using Pandas and scikit-learn.</li>
+                        <li>Planned and led the project to a high standard.</li>
+                    </ul>
+                    <br />
+                    <a className="linkButton" href='https://github.com/Skues/AI-Train-Chatbot' target='_blank'>Link</a>
                 </li>
                 <li className='individualProject'>
                     <h2>Steam Watch</h2>
-                    <p>For fun backend project used to track my friends playtime on Steam and to, eventually, visually rank it on most time wasted. Gathers friend list data based on someone's SteamID, then tracks all their recently played games (last 2 weeks) and total playtime. After all this data is gathered, they will all be ranked on the most time played and which games they all played. Built in Go and uses SteamAPI for the data.</p>
-                    <a href='https://github.com/Skues/steam-watch' target='_blank'>Link</a>
+                    <p>For fun backend project used to track my friends playtime on Steam and to, eventually, visually rank it on most time wasted.</p>
+                    <ul>
+                        <li>Gathers friend list data based on a SteamID</li>
+                        <li>Identifies most played games:</li>
+                        <ul><li>Within the last 2 weeks</li>
+                            <li>Overall</li>
+                        </ul>
+                        <li>Ranked on the most time played and displays which games were played</li>
+                        <li>Build in Go using SteamAPI</li>
+                    </ul>
+                    <br />
+                    <a className="linkButton" href='https://github.com/Skues/steam-watch' target='_blank'>Link</a>
                 </li>
             </ul>
 
@@ -112,10 +150,16 @@ function Connect() {
         <div className='connectDiv'>
             <h1 id='connectAnchor'>Connect</h1>
             <p>Feel free to connect or contact me!</p>
-            <a href='mailto:alfieskues2006@gmail.com'>Email</a>
-            <a href='https://github.com/Skues'>GitHub</a>
-            <a href='https://uk.linkedin.com/in/alfie-skues-16240329a'>LinkedIn</a>
-        </div>
+            <a href='https://github.com/Skues' target='_blank'>
+                <img className="githubLogo" src='/pictures/githublogo.svg' />
+            </a>
+            <a href='mailto:alfieskues2006@gmail.com' target='_blank'>
+                <img className='emailLogo' src='/pictures/emailLogo.svg' />
+            </a>
+            <a href='https://uk.linkedin.com/in/alfie-skues-16240329a' target='_blank'>
+                <img className='linkedinLogo' src='/pictures/linkedin-app-white-icon.svg' />
+            </a>
+        </div >
     )
 }
 
