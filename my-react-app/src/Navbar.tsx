1a1bd8b3f134d3920569a9783ab1e924bdb6978e
Skues <alfieskues2006@gmail.com>
2025-09-27 15:55:22 +0100

    Mobile navbar completed with a hamburger menu

--- my-react-app/src/Navbar.tsx
+++ my-react-app/src/Navbar.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 
 function Navbar() {
+    let [isOpen, setIsOpen] = useState(false)
+    function toggleHamburguer() {
+        setIsOpen(!isOpen)
+
+    }
     return (
         <nav className='navbar'>
-            <div className='navbar-left'>
-                <p style={{ fontWeight: "bold" }}>AWSkues</p>
+            <div className="navbar-header">
+                <div className='navbar-left'>
+                    <p className="logo" style={{ fontWeight: "bold" }}>AWSkues</p>
+                </div>
+                <button className="hamburger" aria-label="Toggle Navigation" onClick={toggleHamburguer}>
+                    ☰
+                </button>
             </div>
-            <div className='navbar-right'>
+            <div className={`navbar-right ${isOpen ? "open" : ""}`}>
                 <ul className='navbar-links'>
                     <li>
                         <a className='hoverUnderline' href='/'>Home</a>
